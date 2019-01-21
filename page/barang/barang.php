d4ac0913a2ed12dbad77e7ca9ea180c3f36c0365
anjasmadani <43619233+anjasmadani@users.noreply.github.com>
2019-01-22 01:15:55 +0700

    Add files via upload

---
+++ page/barang/barang.php
@@ -0,0 +1,67 @@
+<!-- Basic Examples -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h1>
+                                <b>DATA BARANG</b>
+                            </h1>
+                        
+                        </div>
+                        <div class="body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
+                                    <thead>
+                                        <tr>
+                                            <th>Barcode</th>
+                                            <th>Nama Barang</th>
+                                            <th>Stock Barang</th>
+                                            <th>Harga Satuan</th>
+                                            <th>Harga Jual</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>201901001</td>
+                                            <td>T-Shirt</td>
+                                            <td>50</td>
+                                            <td>120,000</td>
+                                            <td>156,000</td>
+                                            <td>
+                                            	<a href="" class="btn btn-success"><i class="material-icons">mode_edit</i></a>
+                                            	<a href="" class="btn btn-danger"><i class="material-icons">delete</i></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>201901002</td>
+                                            <td>Nike Air Jordan</td>
+                                            <td>3</td>
+                                            <td>5,000,000</td>
+                                            <td>7,000,000</td>
+                                            <td>
+                                            	<a href="" class="btn btn-success"><i class="material-icons">mode_edit</i></a>
+                                            	<a href="" class="btn btn-danger"><i class="material-icons">delete</i></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>201901003</td>
+                                            <td>Yezzy</td>
+                                            <td>5</td>
+                                            <td>1,000,000</td>
+                                            <td>1,500,000</td>
+                                            <td>
+                                            	<a href="" class="btn btn-success"><i class="material-icons">mode_edit</i></a>
+                                            	<a href="" class="btn btn-danger"><i class="material-icons">delete</i></a>
+                                            </td>
+                                        </tr>                                       
+                                    </tbody>
+                                </table>
+                                <a href="" class="btn btn-primary"><i class="material-icons">add_circle_outline</i>Tambah Data</a>
+                                <a href="page/barang/cetakb.php" target="blank" class="btn btn-primary"><i class="material-icons">local_printshop</i>Cetak</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                        
---
+++ page/user/user.php
@@ -0,0 +1,40 @@
+<!-- Basic Examples -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h1>
+                                <b>DATA USER</b>
+                            </h1>
+                        
+                        </div>
+                        <div class="body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
+                                    <thead>
+                                        <tr>
+                                            <th>ID Pegawai</th>
+                                            <th>Nama Pegawai</th>                                          
+                                            <th>Alamat</th>
+                                            <th>Username</th>
+                                            <th>Password</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>201902001</td>
+                                            <td>Anjas Madani Tahir</td>
+                                            <td>Yogyakarta</td>
+                                            <td>anjasmadani</td>
+                                            <td>123456</td>
+                                            <td>
+                                            	<a href="" class="btn btn-success"><i class="material-icons">mode_edit</i></a>
+                                            	<a href="" class="btn btn-danger"><i class="material-icons">delete</i></a>
+                                            </td>
+                                        </tr>                                       
+                                    </tbody>
+                                </table>
+                                <a href="?page=barang&aksi=tambah" class="btn btn-primary"><i class="material-icons">add_circle_outline</i>Tambah Data</a>
+                            </div>
+                        
